84af58774ac00bedfe2b9c5ca1bc682d3bdd97c2
pandi <44537591+EpicNyanPandi@users.noreply.github.com>
2022-06-02 10:20:05 -0500

    separated history length from goertzel length, disabled Recorder freezing

--- Recorder.cpp
+++ Recorder.cpp
@@ -4,19 +4,21 @@
 
 // useful: https://miniaudio.docsforge.com/master/api/ma_device/
 
-av::Recorder::Recorder(size_t sample_history_length) {
+av::Recorder::Recorder(size_t min_history_samples) {
 	ma_device_config config = ma_device_config_init(ma_device_type_capture);
 	config.sampleRate = 0;
 	config.dataCallback = data_callback;
 	config.pUserData = this;
 	config.capture.format = ma_format_f32;
-	config.capture.channels = 1; // todo support graphics with multiple channels
+	config.capture.channels = 1; // todo multiple channels
 	if (ma_device_init(nullptr, &config, &device) != MA_SUCCESS)
 		throw std::runtime_error("miniaudio failed to initialize device");
+	// round the history length up to the nearest period_size_in_frames
+	size_t sample_history_length =
+		(min_history_samples + frames_per_period - 1) / frames_per_period * frames_per_period;
 	sample_history = new float[sample_history_length](); // zero-initialized
-	sample_history_begin = sample_history_ptr = sample_history;
-	// round the size of the queue down to the nearest period_size_in_frames
-	sample_history_end = sample_history_begin + sample_history_length / frames_per_period * frames_per_period;
+	sample_history_ptr = sample_history_begin = sample_history;
+	sample_history_end = sample_history_begin + sample_history_length;
 }
 
 av::Recorder::~Recorder() {
@@ -57,8 +59,13 @@ void av::Recorder::stop() {
 
 float av::Recorder::get_sample_rate() const { return static_cast<float>(device.sampleRate); }
 
-void av::Recorder::data_callback(ma_device *pDevice, void *const pOutput, void const *const pInput, ma_uint32 frameCount) {
-//	todo maybe implement downmixing manually https://dsp.stackexchange.com/q/3581
+void av::Recorder::data_callback(
+	ma_device *pDevice,
+	void *const pOutput,
+	void const *const pInput,
+	ma_uint32 frameCount
+) {
+//	todo maybe implement downmixing manually: https://dsp.stackexchange.com/q/3581
 	auto *rec = static_cast<Recorder *>(pDevice->pUserData);
 	float *sample_history_ptr_new = rec->sample_history_ptr + frameCount;
 	if (sample_history_ptr_new >= rec->sample_history_end) {
--- Recorder.h
+++ Recorder.h
@@ -6,9 +6,10 @@
 namespace av {
 	class Recorder {
 	public:
-		float *sample_history_begin, *sample_history_end;
+		// todo const
+		float *sample_history_begin, *sample_history_end, *sample_history_ptr;
 
-		explicit Recorder(size_t sample_history_length);
+		explicit Recorder(size_t min_history_samples);
 
 		~Recorder();
 
@@ -21,9 +22,9 @@ namespace av {
 		[[nodiscard]] float get_sample_rate() const;
 
 	private:
-		float *sample_history, *sample_history_ptr;
 		ma_device device;
 		ma_uint32 const &frames_per_period = device.capture.internalPeriodSizeInFrames;
+		float *sample_history;
 
 		// when there's new audio, this function will get called
 		static void data_callback(ma_device *pDevice, void *pOutput, void const *pInput, ma_uint32 frameCount);
