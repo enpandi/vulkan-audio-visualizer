d00f07a2516c2bb4cf27e33f60a64700f71074b9
pandi <44537591+EpicNyanPandi@users.noreply.github.com>
2022-06-02 10:22:28 -0500

    light rewriting, e.g. multiplying magnitudes by frequency

--- main.cpp
+++ main.cpp
@@ -2,8 +2,17 @@
 #include "Recorder.h"
 
 #include <cmath>
-#include <iomanip>
 #include <iostream>
+
+
+constexpr long double lo_frequency = 55.0l;
+constexpr long double hi_frequency = 3520.0l;
+constexpr unsigned int num_goertzel_samples = 480 * 16;
+constexpr unsigned int num_history_samples = num_goertzel_samples * 16;
+constexpr unsigned int freqs_per_octave = 48;
+constexpr float dampening_factor = 0.95f; // higher values mean the max volume (for normalization) will decrease slower
+constexpr unsigned int target_fps = 90;
+
 
 // perf doesn't work
 namespace timer {
@@ -15,31 +24,15 @@ namespace timer {
 	}
 
 	// stop stopwatch and print message
-	void stop(char const *message) {
+	void stop() {
 		auto stop_time = std::chrono::steady_clock::now();
 		auto nanos = std::chrono::duration_cast<std::chrono::nanoseconds>(stop_time - start_time).count();
-		std::cout << std::right << std::setw(15) << static_cast<long double>(nanos) / 1e9 << " s - " << message << std::endl;
-	}
-
-	// for counting fps
-	std::chrono::time_point<std::chrono::steady_clock> prev_frame_time;
-	uint64_t frame_count = 0;
-	uint64_t prev_frame_count;
-
-	// call this once per frame
-	void fps() {
-		if (!frame_count)
-			prev_frame_time = std::chrono::steady_clock::now();
-		++frame_count;
-		auto now = std::chrono::steady_clock::now();
-		auto nanos = std::chrono::duration_cast<std::chrono::nanoseconds>(now - prev_frame_time).count();
-		if (nanos > 1000000000) { // one second
-			prev_frame_time = now;
-			std::cout << frame_count - prev_frame_count << ' ';
-			prev_frame_count = frame_count;
-		}
+		std::cout << 1000000000 / nanos << ' ';
 	}
 }
+
+// todo a lot of the constants are calculated in a bad way and a lot of the code style is questionable
+// todo but it's the main file so i don't care as much -- users are invited to rewrite
 
 // todo cache locality
 // todo simd
@@ -47,46 +40,40 @@ namespace timer {
 // use long double for all precomputations
 // switch to float for the main part
 
-// "normalized device coordinates" https://stackoverflow.com/q/48036410
+std::vector<long double> generate_frequencies(
+	long double lo_frequency,
+	long double hi_frequency,
+	unsigned int frequencies_per_octave,
+	long double round_to_frequency = 440.0l
+) {
+	if (std::isnan(lo_frequency) || lo_frequency <= 0.0l)
+		throw std::invalid_argument("lo_frequency must be positive");
+	if (std::isnan(hi_frequency) || hi_frequency <= 0.0l)
+		throw std::invalid_argument("hi_frequency must be positive");
+	if (std::isnan(round_to_frequency) || round_to_frequency <= 0.0l)
+		throw std::invalid_argument("round_to_frequency must be positive");
+	if (!frequencies_per_octave)
+		throw std::invalid_argument("frequencies_per_octave must be positive");
+	if (lo_frequency > hi_frequency)
+		throw std::invalid_argument("lo_frequency must be less than or equal to hi_frequency");
+	// note_index == 0 corresponds to round_to_frequency
+	// note_index == frequencies_per_octave corresponds to round_to_frequency*2.0l
+	long long lo_note_index = std::llround(std::log2(lo_frequency / round_to_frequency) * frequencies_per_octave);
+	long long hi_note_index = std::llround(std::log2(hi_frequency / round_to_frequency) * frequencies_per_octave);
+	std::vector<long double> frequencies;
+	frequencies.reserve(hi_note_index - lo_note_index + 1);
+	for (long long note_index = lo_note_index; note_index <= hi_note_index; ++note_index)
+		frequencies.emplace_back(
+			std::exp2(static_cast<long double>(note_index) / frequencies_per_octave) * round_to_frequency);
+	return frequencies; // std::ranges?
+}
 
-// frequency at the coordinate y=0
-constexpr long double center_frequency = 440.0l;
-
-// 24 -- each frequencies is spaced a quarter-tone apart
-constexpr unsigned int frequencies_per_octave = 24;
-
-// multiplicative difference between adjacent frequencies
-long double const frequency_factor = powl(2.0l, 1.0l / frequencies_per_octave);
-
-// visual space between adjacent frequencies, based on normalized device coordinates
-constexpr long double viewport_y_interval = 0.01l;
-
-std::vector<long double> frequencies;
-std::vector<long double> y_values;
-size_t num_freqs; // frequencies.size()
-
-void generate_frequencies_and_y_values() {
-	std::vector<long double> lo, hi;
-	long double y_dist = viewport_y_interval;
-	long double lo_freq = center_frequency / frequency_factor;
-	long double hi_freq = center_frequency * frequency_factor;
-	while (y_dist < 1.0l) {
-		lo.push_back(lo_freq);
-		hi.push_back(hi_freq);
-		lo_freq /= frequency_factor;
-		hi_freq *= frequency_factor;
-		y_dist += viewport_y_interval;
-	}
-	int n = static_cast<int>(lo.size());
-	frequencies.clear();
-	frequencies.reserve(n * 2 + 1);
-	frequencies.insert(frequencies.end(), lo.rbegin(), lo.rend());
-	frequencies.push_back(center_frequency);
-	frequencies.insert(frequencies.end(), hi.begin(), hi.end());
-	y_values.clear();
-	for (int i = n; i >= -n; --i)
-		y_values.push_back(viewport_y_interval * i);
-	num_freqs = frequencies.size();
+std::vector<float> generate_y_values(unsigned int n) {
+	std::vector<float> y_values;
+	y_values.reserve(n);
+	for (unsigned int i = n; i >= 1; --i)
+		y_values.push_back(i * 2.0l / (n + 1) - 1.0l);
+	return y_values;
 }
 
 // https://tauday.com/tau-manifesto
@@ -108,47 +95,69 @@ constexpr long double tau = std::numbers::pi_v<long double> * 2.0l;
  * squared magnitude = s0**2 + s1**2 - s0*s1*g
  * todo how to test this
  */
-std::vector<float> goertzel_coeffs;
-
-void generate_goertzel_constants(long double sample_rate) {
-	goertzel_coeffs.clear();
+std::vector<float> generate_goertzel_constants(std::vector<long double> const &frequencies, long double sample_rate) {
+	std::vector<float> goertzel_constants;
+	goertzel_constants.reserve(frequencies.size());
 	for (long double f : frequencies)
-		goertzel_coeffs.emplace_back(2.0l * cosl(tau * f / sample_rate));
+		goertzel_constants.emplace_back(2.0l * cos(f * tau / sample_rate));
+	return goertzel_constants;
 }
 
 std::vector<float> s0, s1, s2;
-std::vector<float> mag_sq; // squared magnitudes (the outputs of the goertzel algorithm)
-void compute_goertzel(av::Recorder const &rec) {
+std::vector<float> mag; // squared magnitudes (the outputs of the goertzel algorithm)
+void compute_goertzel(av::Recorder &rec, std::vector<float> const &goertzel_constants) {
 	// todo SSE/AVX ? also check if the compiler does it automatically
 /*
 	for (size_t i = 0; i < num_freqs; ++i) {
-		float const &g = goertzel_coeffs[i];
+		float const &g = goertzel_constants[i];
 		float s0 = 0.0f, s1 = 0.0f, s2 = 0.0f;
 		for (float *p = rec.sample_history_begin; p != rec.sample_history_end; ++p) {
 			s0 = g * s1 - s2 + *p;
 			s2 = s1;
 			s1 = s0;
 		} // it's fine if the signal isn't in order
-		mag_sq[i] = s1 * s1 + s2 * s2 - s1 * s2 * g;
+		mag[i] = s1 * s1 + s2 * s2 - s1 * s2 * g;
 	}
 */
 	std::ranges::fill(s1, 0.0f);
 	std::ranges::fill(s2, 0.0f);
-	for (float *p = rec.sample_history_begin; p != rec.sample_history_end; ++p) {
-		float sample = *p;
-		for (size_t i = 0; i < num_freqs; ++i)
-			s0[i] = goertzel_coeffs[i] * s1[i] - s2[i] + sample;
-		s2 = s1;
-		s1 = s0;
-		for (size_t i = 0; i < num_freqs; ++i)
-			mag_sq[i] = s1[i] * s1[i] + s2[i] * s2[i] - s1[i] * s2[i] * goertzel_coeffs[i];
+	float *ptr = rec.sample_history_ptr; // prevent race conditions
+	if (ptr - num_goertzel_samples < rec.sample_history_begin) {
+		for (float *p = rec.sample_history_end - (ptr - rec.sample_history_begin); p != rec.sample_history_end; ++p) {
+			float sample = *p;
+			for (size_t i = 0; i < goertzel_constants.size(); ++i)
+				s0[i] = goertzel_constants[i] * s1[i] - s2[i] + sample;
+			s2 = s1;
+			s1 = s0;
+			for (size_t i = 0; i < goertzel_constants.size(); ++i)
+				mag[i] = s1[i] * s1[i] + s2[i] * s2[i] - s1[i] * s2[i] * goertzel_constants[i];
+		}
+		for (float *p = rec.sample_history_begin; p != ptr; ++p) {
+			float sample = *p;
+			for (size_t i = 0; i < goertzel_constants.size(); ++i)
+				s0[i] = goertzel_constants[i] * s1[i] - s2[i] + sample;
+			s2 = s1;
+			s1 = s0;
+			for (size_t i = 0; i < goertzel_constants.size(); ++i)
+				mag[i] = s1[i] * s1[i] + s2[i] * s2[i] - s1[i] * s2[i] * goertzel_constants[i];
+		}
+	} else {
+		for (float *p = ptr - num_goertzel_samples; p != ptr; ++p) {
+			float sample = *p;
+			for (size_t i = 0; i < goertzel_constants.size(); ++i)
+				s0[i] = goertzel_constants[i] * s1[i] - s2[i] + sample;
+			s2 = s1;
+			s1 = s0;
+			for (size_t i = 0; i < goertzel_constants.size(); ++i)
+				mag[i] = s1[i] * s1[i] + s2[i] * s2[i] - s1[i] * s2[i] * goertzel_constants[i];
+		}
 	}
 }
 
 std::vector<av::Presenter::Vertex::Color> make_rainbow(size_t n) {
 	std::vector<av::Presenter::Vertex::Color> rainbow(n);
 	for (size_t i = 0; i < n; ++i) {
-		float h = i * 6.0f / n;
+		float h = i * 6.0l / n;
 		switch ((size_t) h) {
 			case 0:
 				rainbow[i].r = 1.0f;
@@ -185,42 +194,74 @@ std::vector<av::Presenter::Vertex::Color> make_rainbow(size_t n) {
 	return rainbow;
 }
 
-constexpr float dampening_factor = 0.95f; // higher values mean the max volume (for normalization) will decrease slower
-constexpr unsigned int target_fps = 90;
-constexpr unsigned int target_nanoseconds_per_frame = 1000000000 / target_fps;
 constexpr unsigned long long target_nanoseconds_per_rainbow_cycle = 8e9;
-//constexpr unsigned int target_nanoseconds_per_frame = 0;
+constexpr unsigned int target_nanoseconds_per_frame = 1000000000 / target_fps;
 static_assert(0.0f < dampening_factor && dampening_factor < 1.0f);
 
 int main() {
 	try {
-		av::Recorder rec(480 * 8);
-		generate_frequencies_and_y_values();
-		generate_goertzel_constants(rec.get_sample_rate());
+		// todo adjust these
+		std::vector<long double> frequencies = generate_frequencies(lo_frequency, hi_frequency, freqs_per_octave);
+		size_t num_freqs = frequencies.size();
+		std::vector<float> y_values = generate_y_values(num_freqs);
+		av::Recorder rec(num_history_samples);
+		std::vector<float> goertzel_constants = generate_goertzel_constants(frequencies, rec.get_sample_rate());
+		{
+			std::cout << "frequencies:";
+			for (long double frequency : frequencies)
+				std::cout << ' ' << frequency;
+			std::cout << std::endl << std::endl;
+			std::cout << "y values:";
+			for (float y_value : y_values)
+				std::cout << ' ' << y_value;
+			std::cout << std::endl << std::endl;
+			std::cout << "goertzel constants:";
+			for (float goertzel_constant : goertzel_constants)
+				std::cout << ' ' << goertzel_constant;
+			std::cout << std::endl << std::endl;
+		}
 		s0.resize(num_freqs);
 		s1.resize(num_freqs);
 		s2.resize(num_freqs);
-		mag_sq.resize(num_freqs);
+		mag.resize(num_freqs);
 
 		using Vertex = av::Presenter::Vertex;
 		std::vector<Vertex> vertices;
-		vertices.reserve(num_freqs);
+		vertices.reserve(num_freqs * 2 + 4);
+		vertices.emplace_back(Vertex{{-1.0f, +1.0f},
+		                             {},});
+		vertices.emplace_back(Vertex{{+1.0f, +1.0f},
+		                             {},});
 		for (size_t i = 0; i < num_freqs; ++i) {
 			vertices.emplace_back(
 				Vertex{
-					{(i & 1) * 2.0f - 1.0f, static_cast<float>(y_values[i])},
+					{-1.0f, y_values[i]},
+					{},
+				}
+			);
+			vertices.emplace_back(
+				Vertex{
+					{+1.0f, y_values[i]},
 					{},
 				}
 			);
 		}
+		vertices.emplace_back(Vertex{{-1.0f, -1.0f},
+		                             {},});
+		vertices.emplace_back(Vertex{{+1.0f, -1.0f},
+		                             {},});
+
+		for (auto &&v : vertices) {
+			std::cout << v.position.x << ',' << v.position.y << '\n';
+		}
 
 		// a lot of this work can probably be put in the shader todo
 
-		std::vector rainbow = make_rainbow(frequencies_per_octave);
+		std::vector rainbow = make_rainbow(freqs_per_octave);
 
-		timer::start();
-		av::Presenter presenter(vertices.size(), true, "av");
-		timer::stop("initialization");
+//		timer::start();
+		av::Presenter presenter(240, 800, "av", false, vertices);
+//		timer::stop();
 
 		rec.start();
 		// relevant to normalization https://en.wikipedia.org/wiki/Parseval%27s_theorem
@@ -228,22 +269,37 @@ int main() {
 		std::chrono::steady_clock::time_point frame_start = std::chrono::steady_clock::now();
 		std::chrono::steady_clock::time_point rainbow_stage_start = frame_start;
 		float max_mag = 0.0f;
-//		size_t rainbow_offset = 0; // cycle through the colors
+		size_t rainbow_offset = 0; // cycle through the colors
 		while (presenter.is_running()) {
-			compute_goertzel(rec);
-			max_mag = std::max(max_mag * dampening_factor, *std::max_element(mag_sq.begin(), mag_sq.end()));
+
+			timer::start();
+
+			compute_goertzel(rec, goertzel_constants);
+			for (size_t i = 0; i < num_freqs; ++i)
+				mag[i] *= frequencies[i];
+			max_mag = std::max(max_mag * dampening_factor, *std::max_element(mag.begin(), mag.end()));
 			for (size_t i = 0; i < num_freqs; ++i) {
-//				Vertex::Color const &color = rainbow[(i + rainbow_offset) % rainbow.size()];
-				Vertex::Color const &color = rainbow[i % rainbow.size()];
-				vertices[i].color.r = color.r * mag_sq[i] / max_mag;
-				vertices[i].color.g = color.g * mag_sq[i] / max_mag;
-				vertices[i].color.b = color.b * mag_sq[i] / max_mag;
+				Vertex::Color const &color = rainbow[(i + rainbow_offset) % rainbow.size()];
+//				Vertex::Color const &color = rainbow[i % rainbow.size()];
+				bool yes = (i == 0 && mag[0] > mag[1])
+				           || (i == num_freqs - 1 && mag[num_freqs - 1] > mag[num_freqs - 2])
+				           || (mag[i] > mag[i - 1] && mag[i] > mag[i + 1]);
+				if (yes || true) {
+					vertices[i * 2 + 2].color = vertices[i * 2 + 3].color = {
+						color.r * mag[i] / max_mag,
+						color.g * mag[i] / max_mag,
+						color.b * mag[i] / max_mag,
+					};
+				} else
+					vertices[i * 2 + 2].color = vertices[i * 2 + 3].color = {0.0f, 0.0f, 0.0f};
 			}
 
 			presenter.set_vertices(vertices.data());
 			presenter.draw_frame();
 
-			timer::fps();
+			timer::stop();
+
+//			timer::fps();
 			std::chrono::steady_clock::time_point frame_end;
 			while (
 				std::chrono::duration_cast<std::chrono::nanoseconds>(
@@ -251,12 +307,13 @@ int main() {
 				).count() < target_nanoseconds_per_frame);
 			frame_start = frame_end;
 
-//			if (std::chrono::duration_cast<std::chrono::nanoseconds>(
-//				(frame_end = std::chrono::steady_clock::now()) - rainbow_stage_start
-//			).count() > target_nanoseconds_per_rainbow_cycle / rainbow.size()) {
-//				rainbow_stage_start = frame_end;
+			if (std::chrono::duration_cast<std::chrono::nanoseconds>(
+				(frame_end = std::chrono::steady_clock::now()) - rainbow_stage_start
+			).count() > target_nanoseconds_per_rainbow_cycle / rainbow.size()) {
+				rainbow_stage_start = frame_end;
 //				++rainbow_offset;
-//			}
+			}
+
 		}
 	} catch (std::system_error &err) {
 		std::cerr << "std::system_error: code " << err.code() << ": " << err.what() << std::endl;
