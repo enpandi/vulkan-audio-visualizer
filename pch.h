144a771bef11e934cfebb418886829844c78cbed
pandi <44537591+EpicNyanPandi@users.noreply.github.com>
2022-06-06 19:01:47 -0500

    precompiled headers kinda

---
+++ pch.h
@@ -0,0 +1,19 @@
+#ifndef AUDIO_VISUALIZER_PCH_H
+#define AUDIO_VISUALIZER_PCH_H
+
+#include <miniaudio/miniaudio.h>
+
+#define VULKAN_HPP_NO_CONSTRUCTORS
+#define VULKAN_HPP_NO_SETTERS
+#define VKFW_NO_STRUCT_CONSTRUCTORS
+#include <vkfw/vkfw.hpp>
+#include <vulkan/vulkan_raii.hpp>
+
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <optional>
+#include <ranges>
+#include <set>
+
+#endif //AUDIO_VISUALIZER_PCH_H
